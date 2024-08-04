b372b248c965e6d9abfb41ce7b3034f441e79e64
Umoh Udonwa <udonwajnr10@gmail.com>
2024-08-04 16:41:37 -0700

    commi

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+const port = 3000
+
+app.listen(port,()=>{
+    console.log("I am Back")
+})
