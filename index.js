cf39bd31a3776a7c20376d63fbb6bf8b6ca92ee5
Umoh Udonwa <udonwajnr10@gmail.com>
2024-09-01 10:13:03 -0700

    commit

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const calender = require("./controllers/calenderGenerator")
 let cors = require("cors")
 // const email = require("./middleware/email")
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // 
 app.use("/api/hospital",require("./route/hospitalAuthenticationRoute.js"))
 // 
