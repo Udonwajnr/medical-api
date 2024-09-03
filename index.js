ccb69e06cacdfcaea5255e828bef416582090ede
Umoh Udonwa <udonwajnr10@gmail.com>
2024-09-03 03:13:49 -0700

    commit

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const colors = require("colors")
 const calender = require("./controllers/calenderGenerator")
 let cors = require("cors")
 let cookieParser = require("cookie-parser")
-// const email = require("./middleware/email")
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+  };
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // 
